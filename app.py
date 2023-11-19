65e35957f7d9d5913711132574c4f020f452b215
Anatolie Poiata <54416834+AnatolPoiata@users.noreply.github.com>
2023-11-19 17:33:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def trip_planner_section():
 				candidates = requests.post(itinerary_url+"itinerary_candidates", json=user_input, headers= header)
 
 				candidates = str(candidates.content)
-				print(candidates)
+				st.write(candidates)
 
 				st.write(json.loads(candidates))
 
